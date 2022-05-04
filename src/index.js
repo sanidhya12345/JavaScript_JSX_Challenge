eff5bede01e6eff719e369207ce06fae8c78b220
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-05-04 18:35:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import react from "react";
+import ReactDOM from "react-dom";
+
+const fname = "Sanidhya";
+const lname = "Varshney";
+const currentyear = new Date();
+ReactDOM.render(
+  <div>
+    <p>Created by {`${fname}${lname}`}</p>
+    <p>Copyright {currentyear.getFullYear()}</p>
+  </div>,
+  document.getElementById("root")
+);
